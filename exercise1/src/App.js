eca600d6712d784cb13f72973eb0025c43c56dc3
talyh <taly.hotimsky@gmail.com>
2018-02-10 16:48:09 -0500

    Generated the data needed

--- exercise1/src/App.js
+++ exercise1/src/App.js
@@ -4,6 +4,90 @@ import './App.css';
 
 class App extends Component {
   render() {
+
+    const data = [
+      {
+        "first": "Heath",
+        "last": "Richmond",
+        "age": 27,
+        "isBestFriend": true
+      },
+      {
+        "first": "Cox",
+        "last": "Estrada",
+        "age": 27,
+        "isBestFriend": true
+      },
+      {
+        "first": "Norma",
+        "last": "Mccoy",
+        "age": 35,
+        "isBestFriend": false
+      },
+      {
+        "first": "Wallace",
+        "last": "Montoya",
+        "age": 27,
+        "isBestFriend": false
+      },
+      {
+        "first": "Concepcion",
+        "last": "Mccarthy",
+        "age": 22,
+        "isBestFriend": true
+      },
+      {
+        "first": "Albert",
+        "last": "Wade",
+        "age": 25,
+        "isBestFriend": true
+      },
+      {
+        "first": "Sykes",
+        "last": "Gonzalez",
+        "age": 29,
+        "isBestFriend": false
+      },
+      {
+        "first": "Alisa",
+        "last": "Ryan",
+        "age": 30,
+        "isBestFriend": true
+      },
+      {
+        "first": "Holmes",
+        "last": "Bright",
+        "age": 28,
+        "isBestFriend": false
+      },
+      {
+        "first": "Estrada",
+        "last": "Pittman",
+        "age": 25,
+        "isBestFriend": false
+      }
+    ]
+
+    const getNames = (array) => {
+      return array.map(({ first, last }) => {
+        let name = first ? `${first}` : '';
+        name += last ? ` ${last}` : '';
+        return name;
+      })
+    };
+
+    const friendNames = getNames(data);
+    console.log(friendNames);
+
+    const bestFriends = data.filter(({ isBestFriend }) => isBestFriend);
+    console.log(bestFriends);
+
+    const bestFriendsNames = getNames(bestFriends);
+    console.log(bestFriendsNames);
+
+    const bestFriendsAverageAge = bestFriends.reduce((accum, curr) => accum += curr.age, 0) / bestFriends.length;
+    console.log(bestFriendsAverageAge);
+
     return (
       <div className="App">
         <header className="App-header">
