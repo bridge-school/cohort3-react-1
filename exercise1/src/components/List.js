7b00c1df5a35743b8b958e78778ec5e093426b5c
talyh <taly.hotimsky@gmail.com>
2018-02-10 18:04:40 -0500

    Basic styling applied

--- exercise1/src/components/List.js
+++ exercise1/src/components/List.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react'
 
 export default class List extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <div>
-                <h1>{this.props.name}</h1>
+                <h1 className="App-header">{this.props.name}</h1>
                 <ul>
                     {this.props.data.map(item => <li>{item}</li>)}
                 </ul>
-                <div>{this.props.summary}</div>
+                <div className="App-title">{this.props.summary}</div>
             </div>
         )
     }
