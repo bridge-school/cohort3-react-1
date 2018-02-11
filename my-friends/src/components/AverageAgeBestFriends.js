fe7d67010c5608420b4c6a95e49dd3851dacedde
Tahuana <tahuana@gmail.com>
2018-02-11 13:29:43 -0500

    Cleaning the code

--- my-friends/src/components/AverageAgeBestFriends.js
+++ my-friends/src/components/AverageAgeBestFriends.js
@@ -2,13 +2,10 @@ import React from 'react';
 
 export const AverageAgeBestFriends = ({ friendList }) => {
 
-  const bestFriends = friendList.filter( eachFriend => { return eachFriend.isBestFriend })
+  const bestFriends = friendList.filter( eachFriend => { return eachFriend.isBestFriend });
   const totalOfBestFriends = bestFriends.length;
-  const sumOfAges = bestFriends.reduce((sum, friend) => { return sum + friend.age }, 0);
+  const sumOfAges = bestFriends.reduce( (sum, friend) => { return sum + friend.age }, 0);
 
-  return (
-    "The average age of my best friends is: " + sumOfAges / totalOfBestFriends
-  );
-
+  return "The average age of my best friends is: " + sumOfAges / totalOfBestFriends;
 
 };
