70d24bcbe29f40bfb74d96d77a38bbdcbefb9d77
talyh <taly.hotimsky@gmail.com>
2018-02-11 13:22:34 -0500

    Extracted data from App.js into its own file

--- exercise1/src/App.js
+++ exercise1/src/App.js
@@ -1,88 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import List from './components/List';
+import friends from './data/friends.json';
 
 class App extends Component {
+
+  getNames = (array) => {
+    return array.map(({ first, last }) => {
+      let name = first ? `${first}` : '';
+      name += last ? ` ${last}` : '';
+      return name;
+    })
+  };
+
   render() {
 
-    const data = [
-      {
-        "first": "Heath",
-        "last": "Richmond",
-        "age": 27,
-        "isBestFriend": true
-      },
-      {
-        "first": "Cox",
-        "last": "Estrada",
-        "age": 27,
-        "isBestFriend": true
-      },
-      {
-        "first": "Norma",
-        "last": "Mccoy",
-        "age": 35,
-        "isBestFriend": false
-      },
-      {
-        "first": "Wallace",
-        "last": "Montoya",
-        "age": 27,
-        "isBestFriend": false
-      },
-      {
-        "first": "Concepcion",
-        "last": "Mccarthy",
-        "age": 22,
-        "isBestFriend": true
-      },
-      {
-        "first": "Albert",
-        "last": "Wade",
-        "age": 25,
-        "isBestFriend": true
-      },
-      {
-        "first": "Sykes",
-        "last": "Gonzalez",
-        "age": 29,
-        "isBestFriend": false
-      },
-      {
-        "first": "Alisa",
-        "last": "Ryan",
-        "age": 30,
-        "isBestFriend": true
-      },
-      {
-        "first": "Holmes",
-        "last": "Bright",
-        "age": 28,
-        "isBestFriend": false
-      },
-      {
-        "first": "Estrada",
-        "last": "Pittman",
-        "age": 25,
-        "isBestFriend": false
-      }
-    ]
-
-    const getNames = (array) => {
-      return array.map(({ first, last }) => {
-        let name = first ? `${first}` : '';
-        name += last ? ` ${last}` : '';
-        return name;
-      })
-    };
-
-    const friendNames = getNames(data);
+    const friendNames = this.getNames(friends);
     // console.log(friendNames);
 
-    const bestFriends = data.filter(({ isBestFriend }) => isBestFriend);
+    const bestFriends = friends.filter(({ isBestFriend }) => isBestFriend);
     // console.log(bestFriends);
 
-    const bestFriendsNames = getNames(bestFriends);
+    const bestFriendsNames = this.getNames(bestFriends);
     // console.log(bestFriendsNames);
 
     const bestFriendsAverageAge = bestFriends.reduce((accum, curr) => accum += curr.age, 0) / bestFriends.length;
